4fa63c6ac45acfa7d7f1afa7d003a523cebafb2b
Daniel Baykov <danibaikov@abv.bg>
2023-01-10 17:54:29 +0200

    adding 05 - Integer to Hex and Binary

---
+++ 01. Java Basics/Exercises/src/E05IntegerToHexAndBinary.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class E05IntegerToHexAndBinary {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        System.out.println(Integer.toHexString(num).toUpperCase());
+        System.out.println(Integer.toBinaryString(num));
+    }
+}

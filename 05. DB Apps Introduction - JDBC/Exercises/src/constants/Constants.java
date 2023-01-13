b872f90fa549238a4b44f5c0dcea2d32f6903b2b
Daniel Baykov <danibaikov@abv.bg>
2023-01-13 10:48:21 +0200

    updating SQL Constants class for Exercise Introduction to DB Apps

--- 05. DB Apps Introduction - JDBC/Exercises/src/constants/Constants.java
+++ 05. DB Apps Introduction - JDBC/Exercises/src/constants/Constants.java
@@ -12,6 +12,22 @@ public final class Constants {
     public static final String TABLE_VILLAINS = "villains";
     public static final String TABLE_MINIONS_VILLAINS = "minions_villains";
 
+    public static final String DATABASE_CREATED_SUCCESSFULLY = "Database created successfully";
+    public static final String ERROR_CREATING_DATABASE = "Error creating database";
+    public static final String DATABASE_ERROR = "Database error";
+    public static final String TABLE_CREATED_SUCCESSFULLY = "Table created successfully";
+    public static final String ERROR_CREATING_TABLE = "Error creating table";
+    public static final String DATA_INSERTED_INTO_TABLE = "Data inserted into table";
+    public static final String ERROR_INSERTING_DATA_INTO_TABLE = "Error inserting data into table";
+    public static final String INVALID_VILLAIN_ID = "No villain with ID %d exists in the database";
+    public static final String NO_MINIONS = "<no minions>";
+    public static final String VILLAIN = "Villain: ";
+    public static final String ENTER_VILLAIN_ID = "Enter villain id: ";
+    public static final String VILLAIN_ADDED = "Villain %s was added to the database%n";
+    public static final String TOWN_ADDED = "Town %s was added to the database%n";
+    public static final String ADDED_MINION_TO_VILLAIN = "Successfully added %s to be minion of %s";
+    public static final String NO_TOWN_NAMES_WERE_AFFECTED = "No town names were affected";
+
     public static final String INPUT_SEPARATOR = "\\s+";
     public static final String OUTPUT_SEPARATOR = " ";
 

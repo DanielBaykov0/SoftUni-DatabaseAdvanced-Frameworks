e4e6ef55d278983578caf4ff41f9f72adde71052
Daniel Baykov <danibaikov@abv.bg>
2023-01-13 10:10:48 +0200

    adding Constants class for Exercise Introduction to DB Apps

---
+++ 05. DB Apps Introduction - JDBC/Exercises/src/constants/Constants.java
@@ -0,0 +1,26 @@
+package constants;
+
+public final class Constants {
+
+    public static final String NAME = "name";
+    public static final String AGE = "age";
+    public static final String ID = "id";
+
+    public static final String DATABASE = "minions_db";
+    public static final String TABLE_TOWNS = "towns";
+    public static final String TABLE_MINIONS = "minions";
+    public static final String TABLE_VILLAINS = "villains";
+    public static final String TABLE_MINIONS_VILLAINS = "minions_villains";
+
+    public static final String INPUT_SEPARATOR = "\\s+";
+    public static final String OUTPUT_SEPARATOR = " ";
+
+    public static final String JDBC = "jdbc";
+    public static final String DRIVER = "mysql";
+    public static final String HOST = "localhost";
+    public static final String PORT = "3306";
+    public static final String USER = "root";
+    public static final String PASSWORD = "!Die69@32";
+
+    public Constants() {}
+}
